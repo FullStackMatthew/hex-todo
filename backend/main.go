525e792600a7c6a6d511ebdacbff8909558a41b3
Alina Litvak <alinalitvak@alinas-air.home>
2023-07-22 11:51:40 -0400

    removed table creation from main.go

--- backend/main.go
+++ backend/main.go
@@ -12,28 +12,12 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// const createTableQuery = `
-// CREATE TABLE IF NOT EXISTS public.todos (
-//     id SERIAL PRIMARY KEY,
-//     title TEXT NOT NULL,
-//     completed BOOLEAN NOT NULL DEFAULT FALSE,
-//     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-//     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-// );
-// `
-
 func main() {
 	// Database connection
 	db, err := sql.Open("postgres", os.Getenv("POSTGRES_CONNECTION_STRING"))
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %s", err)
 	}
-
-	// Create the "todos" table
-	// _, err = db.Exec(createTableQuery)
-	// if err != nil {
-	// 	log.Fatalf("Failed to create the \"todos\" table: %s", err)
-	// }
 
 	// Repository and Interactor setup
 	todoRepository := repository.NewTodoRepository(db)
